e5024dc1549d6cfd7dd1a8934b58875bbae604e7
mesyashapa <mesyashapaa@gmail.com>
2024-12-12 12:21:26 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,7 +6,21 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
+static String fizz(int n) {
+            if (n % 3 == 0 && n % 5 == 0) {
+                return "FizzBuzz";
+            } else if (n % 3 == 0) {
+                return "Fizz";
+            } else if (n % 5 == 0) {
+                return "Buzz";
+            } 
+                return String.valueOf(n);
+            
+        }
+        public static void main(String[] args) {
+            Scanner sc= new Scanner(System.in);
+            int n = sc.nextInt();
+            System.out.println(fizz(n));
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
