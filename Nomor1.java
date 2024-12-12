d543f44ee80a81cb90295e5f61d909aa8dc11958
mesyashapa <mesyashapaa@gmail.com>
2024-12-12 12:36:49 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -6,35 +6,15 @@ public class Nomor1 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-              // Prosedur untuk mencetak deret FizzBuzz
-    public static void printFizzBuzz(int n) {
-        for (int i = 0; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.print("FizzBuzz ");
-            } else if (i % 3 == 0) {
-                System.out.print("Fizz ");
-            } else if (i % 5 == 0) {
-                System.out.print("Buzz ");
-            } else {
-                System.out.print(i + " ");
+   int jumlahGanjil = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0) {
+                jumlahGanjil++;
             }
         }
-    }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Meminta input nilai n dari pengguna
-        System.out.print("Masukan nilai N: ");
-        int n = scanner.nextInt();
-
-        // Memanggil prosedur untuk mencetak deret FizzBuzz
-        printFizzBuzz(n);
-
-        scanner.close();
-    }
-}
-
+        System.out.println("Jumlah bilangan ganjil dari 1 hingga " + N + " adalah " + jumlahGanjil);
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
